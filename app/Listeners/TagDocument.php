be7a71a264fdfe4efe66b0234cd2d0342e5154d6
FelipeMalacarne <FelipeMalacarne@users.noreply.github.com>
2024-10-26 00:39:39 +0000

    Fixes coding style

--- app/Listeners/TagDocument.php
+++ app/Listeners/TagDocument.php
@@ -2,9 +2,8 @@
 
 namespace App\Listeners;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use App\Events\Document\Created as DocumentCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class TagDocument implements ShouldQueue
 {
