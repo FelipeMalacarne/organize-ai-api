b55364e865026e791abe365dc44ba901ac072ec1
FelipeMalacarne <FelipeMalacarne@users.noreply.github.com>
2024-10-31 22:59:06 +0000

    Fixes coding style

--- app/Services/DocumentAI/Enums/ProcessorEnum.php
+++ app/Services/DocumentAI/Enums/ProcessorEnum.php
@@ -4,7 +4,8 @@ namespace App\Services\DocumentAI\Enums;
 
 use Google\Cloud\DocumentAI\V1\Client\DocumentProcessorServiceClient;
 
-enum ProcessorEnum: string {
+enum ProcessorEnum: string
+{
     case Classifier = 'c4b723ed153b647f';
     case Extractor = '9cc1da015e77e6a3';
 
